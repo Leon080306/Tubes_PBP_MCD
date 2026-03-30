f1fabdc952ca4bb7de985da90a2076e674d81266
JenniferAverina <angelaa.ave@gmail.com>
2026-03-30 13:54:41 +0700

    add assets

--- frontend/src/config/AppRoutes.tsx
+++ frontend/src/config/AppRoutes.tsx
@@ -2,14 +2,14 @@ import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 import Layout from '../components/Layout';
 
-const HomePage = lazy(() => import('../pages/customer/HomePage'));
+const MenuList = lazy(() => import('../pages/customer/MenuList.tsx'));
 
 export const AppRoutes = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<HomePage />} />
+                    <Route path="/" element={<MenuList />} />
                 </Route>
             </Routes>
         </Suspense>
